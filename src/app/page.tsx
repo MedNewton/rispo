c0157ca3b073ebc8ea8e8041593db87f4f899c37
MedNewton <med.newton.2013@gmail.com>
2025-08-19 23:42:49 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,20 @@
 // app/page.tsx
 import MobileHeader from "../components/mobileHeader";
 import Sidebar from "../components/sidebar";
+import HomepageContent from "../components/homepageContent";
+
+
 
 export default function HomePage() {
   return (
     <main className="noiseBackground">
       <MobileHeader />
-
-      {/* Fill the viewport: on mobile subtract header (4rem), on md+ use full height */}
       <div className="flex w-[100vw] overflow-hidden h-[calc(100dvh-4rem)] md:h-[100dvh]">
-        {/* Sidebar only on md+ */}
         <div className="hidden md:block">
           <Sidebar />
         </div>
-
-        {/* Scrollable content area (scrollbar hidden) */}
-        <section className="flex-1 overflow-y-auto overflow-x-hidden scrollbar-hide px-4 sm:px-6 md:px-8 py-4 sm:py-6">
-          {/* content goes here */}
+        <section className="flex-1 overflow-y-auto overflow-x-hidden scrollbar-hide px-4 sm:px-6 md:px-8 py-4 flex flex-col justify-end sm:py-6">
+          <HomepageContent />
         </section>
       </div>
     </main>
---
+++ src/components/homepageContent.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+import image2 from "../assets/images/2.webp";
+import image6 from "../assets/images/6.webp";
+import image12 from "../assets/images/12.webp";
+
+export default function HomepageContent() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+      <div className="flex flex-col h-[550px]">
+        <div className="relative flex-1">
+          <Image src={image6} alt="Image 3" fill className="object-cover" />
+        </div>
+        <h6 className="text-2xl font-light mt-4 self-start">
+          Category 1
+        </h6>
+      </div>
+      <div className="flex flex-col h-[550px]">
+        <div className="relative flex-1">
+          <Image src={image12} alt="Image 9" fill className="object-cover" />
+        </div>
+        <h6 className="text-2xl font-light mt-4 self-start">
+          Category 2
+        </h6>
+      </div>
+      <div className="flex flex-col h-[550px]">
+        <div className="relative flex-1">
+          <Image src={image2} alt="Image 17" fill className="object-cover" />
+        </div>
+        <h6 className="text-2xl font-light mt-4 self-start">
+          Category 3
+        </h6>
+      </div>
+    </div>
+  );
+}
