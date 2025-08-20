6b4f0a21da6ab6597b4eae75e748cf0ed220ccf7
MedNewton <med.newton.2013@gmail.com>
2025-08-20 01:17:41 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
         <div className="hidden md:block">
           <Sidebar />
         </div>
-        <section className="flex-1 overflow-y-auto overflow-x-hidden scrollbar-hide px-4 sm:px-6 md:px-8 py-4 sm:py-6 flex flex-col justify-end">
+        <section className="flex-1 overflow-y-auto overflow-x-hidden scrollbar-hide px-4 sm:px-6 md:px-8 py-4 sm:py-6 flex flex-col lg:justify-end">
           <HomepageContent />
           <div className="mt-8 md:hidden">
             <MobileFooter />
